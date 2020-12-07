6c2e7e3c30891eabb3af0a87a8677b707c83e855
Alexander Valencia <alex.valencia@adhocteam.us>
2020-12-06 22:47:23 -0600

    :tada: completed day 05 :sparkle:

---
+++ 2020/05/b.js
@@ -0,0 +1,50 @@
+const input = require("fs").readFileSync("./05/input.txt", "utf8").split("\n");
+
+const findMidpoint = (f) => (a, b) => f((a + b) / 2);
+const findLowerMidpoint = findMidpoint(Math.floor);
+const findUpperMidpoint = findMidpoint(Math.ceil);
+
+const findSeat = ({ max, ranges }) => {
+  let min = 0;
+  let result;
+
+  ranges.forEach((range) => {
+    if (range === "F" || range === "L") {
+      max = findLowerMidpoint(min, max);
+      result = max;
+    } else {
+      min = findUpperMidpoint(min, max);
+      result = min;
+    }
+  });
+
+  return result;
+};
+
+const boardingPasses = [];
+
+input.forEach((line, i) => {
+  const matches = line.match(/((B|F){7})((L|R){3})/);
+  const rows = matches[1].split("");
+  const columns = matches[3].split("");
+
+  const row = findSeat({ max: 127, ranges: rows });
+  const column = findSeat({ max: 7, ranges: columns });
+  const id = row * 8 + column;
+
+  boardingPasses.push({ row, column, id });
+});
+
+const sortedPasses = boardingPasses
+  .map((pass) => pass.id)
+  .sort((a, b) => a - b);
+
+let missingSeat;
+
+sortedPasses.forEach((id, i) => {
+  if (sortedPasses.indexOf(id + 1) == -1 && i < sortedPasses.length - 1) {
+    missingSeat = id + 1;
+  }
+});
+
+console.log(missingSeat); // 565
