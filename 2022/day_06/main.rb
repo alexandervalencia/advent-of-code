1cdbb029953a612f4c8ebacc5ff72fc110396199
Alex Valencia <alex@planningcenter.com>
2022-12-07 16:25:28 -0600

    :tada: solved! :tada:

--- 2022/day_06/main.rb
+++ 2022/day_06/main.rb
@@ -6,4 +6,6 @@ puts "=" * 40
 input = File.read(File.join(__dir__, "input.txt"))
 communication_device = CommunicationDevice.new(input)
 start_of_packet_position = communication_device.start_of_packet_position
+start_of_message_position = communication_device.start_of_message_position
 puts "Solution: start-of-packet position begins at #{start_of_packet_position}"
+puts "Solution: start-of-message position begins at #{start_of_message_position}"
