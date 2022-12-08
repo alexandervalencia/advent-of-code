c338cb279b2ae06fb4410d866780584263ca7142
Alex Valencia <alex@planningcenter.com>
2022-12-07 23:00:11 -0600

    setup day 8

---
+++ 2022/day_08/main.rb
@@ -0,0 +1,7 @@
+require_relative "day_08"
+
+puts "=" * 40
+puts "Advent of Code - Day 08"
+puts "=" * 40
+input = File.read(File.join(__dir__, "input.txt"))
+puts "Solution: TODO"
---
+++ 2022/day_08/test/day_08_test.rb
@@ -0,0 +1,8 @@
+require_relative "../../test_helper"
+require_relative "../day_08"
+
+class Day08Test < Minitest::Test
+  test "wat it do" do
+    skip
+  end
+end
