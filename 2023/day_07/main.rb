449273aa429c040c05e01c65b69918de4e92d08d
Alex Valencia <alex.valencia@me.com>
2023-12-07 17:18:52 -0600

    Day 07

--- 2023/day_07/main.rb
+++ 2023/day_07/main.rb
@@ -4,4 +4,7 @@ puts "=" * 40
 puts "Advent of Code - Day 07"
 puts "=" * 40
 input = File.read(File.join(__dir__, "input.txt"))
-puts "Solution: TODO"
+total_winnings = Day07.new(input).total_winnings
+total_winnings_with_jokers = Day07.new(input, true).total_winnings
+puts "Solution for Part 1 — Total Winnings: #{total_winnings}"
+puts "Solution for Part 1 — Total Winnings with Jokers: #{total_winnings_with_jokers}"
--- 2023/test_helper.rb
+++ 2023/test_helper.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require "minitest/focus"
 require "minitest/pride"
 
 class Minitest::Test
